8c5fec53ff19a4cf56114ff44b51bd4df37dfef7
NicoleKuong <nicolekuong@gmail.com>
2020-01-25 01:38:47 +0100

    step 4

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,14 +1,45 @@
 import React, { Component } from "react";
 
 export default class Quote extends Component {
+  state = {
+    color: "",
+    textDecorationLine: "",
+    fontWeight: ""
+  };
+
+  handleLikes = () => {
+    this.setState({
+      color: "green",
+      textDecorationLine: "",
+      fontWeight: "bold"
+    });
+  };
+
+  handleDislikes = () => {
+    this.setState({
+      color: "red",
+      textDecorationLine: "line-through",
+      fontWeight: ""
+    });
+  };
+
   render() {
     const { quoteText, quoteAuthor } = this.props;
     return (
       <div>
-        <div>{quoteText}</div>
+        <div
+          style={{
+            color: this.state.color,
+            textDecorationLine: this.state.textDecorationLine,
+            fontWeight: this.state.fontWeight
+          }}
+        >
+          {quoteText}
+        </div>
         <br />
         <div>By : {quoteAuthor}</div>
-        <br />
+        <button onClick={this.handleDislikes}>:(</button>
+        <button onClick={this.handleLikes}>:)</button>
       </div>
     );
   }
